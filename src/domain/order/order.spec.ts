6b70c33da444b8a9d788c7f59f75653e550f8aad
Davi <davighiggino_otg@cnc.org.br>
2023-11-29 20:25:36 -0300

    test: implementando os testes unitários de customer e order

---
+++ src/domain/order/order.spec.ts
@@ -0,0 +1,35 @@
+import Order from "./order";
+import OrderItem from "./orderItem";
+
+describe("Order unit test", () => {
+  it("Should throw error when order id is empty", () => {
+    expect(() => {
+      new Order("", "Davi");
+    }).toThrow("id is required");
+  });
+
+  it("Should throw error when order id is empty", () => {
+    expect(() => {
+      new Order("1", "");
+    }).toThrow("customerId is required");
+  });
+
+  it("should throw error when quantity greater than 0", () => {
+    expect(() => {
+      new OrderItem("1", "product", 100, 0);
+    }).toThrow("quantity greater than 0");
+  });
+  it("should throw error when the name of item is empty", () => {
+    expect(() => {
+      new OrderItem("1", "", 100, 1);
+    }).toThrow("name is required");
+  });
+
+  it("Should calculate total order with 2 items", () => {
+    const order = new Order("1", "Davi");
+    const item = new OrderItem("item1", "product1", 100, 2);
+    const item2 = new OrderItem("item1", "product1", 50, 1);
+    order.addItems([item, item2]);
+    expect(order.total()).toBe(250);
+  });
+});
