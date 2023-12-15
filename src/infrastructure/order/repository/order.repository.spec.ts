f72e85a06be1ff0a0ea72f75a6bb2ae53bf24a39
Davi <davighiggino_otg@cnc.org.br>
2023-12-15 10:38:11 -0300

    test: Implementando criação de um order

--- src/infrastructure/order/repository/order.repository.spec.ts
+++ src/infrastructure/order/repository/order.repository.spec.ts
@@ -1,5 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
+import Customer from "../../../domain/customer/customer";
+import Order from "../../../domain/order/order";
+import OrderItem from "../../../domain/order/orderItem";
+import Product from "../../../domain/product/product";
+import CustomerModel from "../../customer/repository/customer.model";
+import ProductModel from "../../product/repository/product.model";
+import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
+import OrderRepository from "./order.repository";
 
 describe("Order repository test", () => {
   let sequelize: Sequelize;
@@ -12,7 +20,12 @@ describe("Order repository test", () => {
       sync: { force: true },
     });
 
-    sequelize.addModels([OrderModel]);
+    sequelize.addModels([
+      OrderModel,
+      OrderItemModel,
+      CustomerModel,
+      ProductModel,
+    ]);
     await sequelize.sync();
   });
 
@@ -20,5 +33,20 @@ describe("Order repository test", () => {
     await sequelize.close();
   });
 
-  it("", () => {});
+  it("Should create a new Order", () => {
+    let product = new Product("1", "Produto 1", "", 249.99);
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      1,
+      product.id
+    );
+    let customer = new Customer("1", "Davi");
+    let order = new Order("1", customer.id);
+    order.addItems([orderItem]);
+
+    const orderRepository = new OrderRepository();
+    orderRepository.create(order);
+  });
 });
